55714229df915a14c3629ed8b2bfd3ceb594702f
Jorge Matos <jisapjisap@gmail.com>
2025-10-25 20:29:10 +0200

    Add Prisma setup with initial schema and database configuration

---
+++ src/lib/db.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = global as unknown as { prisma:PrismaClient }; // Define the global variable
+
+const prisma = globalForPrisma.prisma || new PrismaClient();          // Use the global variable or create a new one
+
+if (process.env.NODE_ENV !== "production") {                          // Only create the prisma client in development
+  globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
+
