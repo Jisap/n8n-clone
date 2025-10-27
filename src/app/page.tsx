105e354d035401826f019c8bde9d86763919162e
Jorge Matos <jisapjisap@gmail.com>
2025-10-27 19:40:20 +0100

    Remove Client component and refactor Page component to utilize authClient for session management; add new authentication route for handling requests.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,42 @@
-import { getQueryClient, trpc } from '@/trpc/server'
-import { Client } from './client'
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
-import { Suspense } from 'react';
+"use client"
 
 
+import { Button } from '@/components/ui/button';
+import { authClient } from '@/lib/auth-client';
 
 
+const Page = () => {
 
-const Page = async() => {
-
-  const queryClient = getQueryClient();
-
-  void queryClient.prefetchQuery(trpc.getUsers.queryOptions()); // PreCarga los datos en la caché de React Query.
+  const { data } = authClient.useSession();
 
   return (
-    <div className='text-red-500'>
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <Suspense fallback={<p>Loading...</p>}>
-          <Client />
-        </Suspense>
-      </HydrationBoundary>
+    <div className='min-h-screen min-w-screen flex items-center justify-center'>
+      {JSON.stringify(data)}
+
+      {data && (
+        <Button onClick={() => authClient.signOut()}>
+          Logout
+        </Button>
+      )}
     </div>
   )
 }
+
+// const Page = async () => {
+
+//   const queryClient = getQueryClient();
+
+//   void queryClient.prefetchQuery(trpc.getUsers.queryOptions()); // PreCarga los datos en la caché de React Query.
+
+//   return (
+//     <div className='text-red-500'>
+//       <HydrationBoundary state={dehydrate(queryClient)}>
+//         <Suspense fallback={<p>Loading...</p>}>
+//           <Client />
+//         </Suspense>
+//       </HydrationBoundary>
+//     </div>
+//   )
+// }
 
 export default Page
