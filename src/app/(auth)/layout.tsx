f733b13a6b85c87372063e974acbc4ee390054e0
Jorge Matos <jisapjisap@gmail.com>
2025-10-30 11:43:37 +0100

    Refactor authentication layout by creating a new AuthLayout component; simplify layout structure and enhance code organization for better maintainability.

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,24 +1,13 @@
-import Image from "next/image"
-import Link from "next/link"
+import AuthLayout from "@/features/auth/components/auth-layout"
+
 
 
 const Layout = ({ children }: { children:React.ReactNode }) => {
   return (
     <>
-      <div className='bg-muted flex flex-col min-h-svh justify-center items-center gap-6 p-6 md:p-10'>
-        <div className='flex w-full max-w-sm flex-col gap-6'>
-          <Link href="/" className='flex items-center gap-2 self-center font-medium'>
-            <Image
-              src="/logos/log.svg"
-              alt="Logo"
-              width={30}
-              height={30}
-            />
-            Nodebase
-          </Link>
-          
-        </div>
-      </div>
+      <AuthLayout>
+        {children}
+      </AuthLayout>
     </>
   )
 }
