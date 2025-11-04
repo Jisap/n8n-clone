80df363067ddc067b2b0c429097ca7624407be4d
Jorge Matos <jisapjisap@gmail.com>
2025-11-04 13:20:41 +0100

    Add Polar authentication integration; include Polar SDK and better-auth dependencies, configure authentication plugins with checkout and portal options, and create polar client for API interactions.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,8 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "@/lib/db";
+import { checkout, polar, portal,webhooks } from "@polar-sh/better-auth";
+import { polarClient } from "./polar"
 
 
 export const auth = betterAuth({      // Crea una instancia de betterAuth
@@ -11,5 +13,25 @@ export const auth = betterAuth({      // Crea una instancia de betterAuth
     enabled: true,
     autoSignIn: true,
     //disableEmailVerification: true 
-  }
+  },
+  plugins: [
+    polar ({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "eebad5fd-ff58-4b61-b8d4-72bf11586329",
+              slug: "pro"
+            }
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+        
+      ]
+    })
+  ]
 });
---
+++ src/lib/polar.ts
@@ -0,0 +1,11 @@
+import { Polar } from "@polar-sh/sdk";
+
+console.log("🔍 POLAR_ACCESS_TOKEN existe:", !!process.env.POLAR_ACCESS_TOKEN);
+console.log("🔍 Longitud del token:", process.env.POLAR_ACCESS_TOKEN?.length);
+console.log("🔍 Primeros 10 caracteres:", process.env.POLAR_ACCESS_TOKEN?.substring(0, 10));
+
+
+export const polarClient = new Polar({
+  accessToken: process.env.POLAR_ACCESS_TOKEN,
+
+})
