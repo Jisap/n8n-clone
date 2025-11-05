19d9348e58a239a59c97b708ccfbc3a6271253b8
Jorge Matos <jisapjisap@gmail.com>
2025-11-05 17:18:56 +0100

    Add workflow management functionality; introduce Workflow model in Prisma schema, implement CRUD operations in workflowsRouter, and integrate random-word-slugs for workflow naming. Update package dependencies accordingly.

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,46 +1,12 @@
 
-import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init';
-import prisma from '@/lib/db';
-import { inngest } from '@/inngest/client';
+import { createTRPCRouter } from '../init';
+;
+import { workflowsRouter } from '@/features/workflows/server/routers';
 
 
 
 export const appRouter = createTRPCRouter({
-  testAi: protectedProcedure
-    .mutation(async() => {
-      await inngest.send({
-        name: "execute/ai",
-      })
-
-      return { sucess: true, message: "Text generated" };
-      
-    }),
-  getUsers: protectedProcedure
-    .query(({ ctx }) => {    
-      return prisma.user.findMany({
-        where: {
-          id: ctx.auth.user.id
-        }
-      });
-    }),
-  createWorkflow: protectedProcedure
-    .mutation(async() => {
-
-      await inngest.send({
-        name: "test/hello.world",
-        data: {
-          email: "test@test.com"
-        }
-      });
-
-      return { sucess: true, message: "Workflow created" };
-
-      
-    }),
-  getWorkflows: protectedProcedure
-    .query(({ ctx }) => {
-      return prisma.workflow.findMany()
-    })
+  workflows: workflowsRouter
 });
 
 
