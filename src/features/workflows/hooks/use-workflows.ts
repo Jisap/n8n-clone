2dc23ac444e313fe2db19418c8f42bcd6003b60f
Jorge Matos <jisapjisap@gmail.com>
2025-11-06 12:46:36 +0100

    Add workflow creation functionality; implement useCreateWorkflow hook for creating new workflows and integrate it into WorkflowsHeader for user interaction, enhancing workflow management capabilities.

--- src/features/workflows/hooks/use-workflows.ts
+++ src/features/workflows/hooks/use-workflows.ts
@@ -1,11 +1,36 @@
 
 import { useTRPC } from "@/trpc/client";
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { useMutation, useQueryClient, useSuspenseQuery } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
+// Hook to fetch all workflows using suspense
 
 export const useSuspenseWorkflows = () => {
 
   const trpc = useTRPC();
 
   return useSuspenseQuery(trpc.workflows.getMany.queryOptions()); // Suspende la renderización del componente hasta que los datos sean recibidos
+}
+
+// Hook to create a new workflow
+
+export const useCreateWorkflow = () => {
+  const router = useRouter();
+  const queryClient = useQueryClient();
+  const trpc = useTRPC();
+
+  return useMutation(
+    trpc.workflows.create.mutationOptions({
+      onSuccess: (data) => {
+        toast.success(`Workflow" ${data.name} "created successfully!`);
+        queryClient.invalidateQueries(trpc.workflows.getMany.queryOptions());
+        router.push(`/workflows/${data.id}`);
+      },
+      onError: (error) => {
+        toast.error(`Failed to create workflow: ${error.message}`);
+        console.error(error);
+      }
+    })
+  )
 }
