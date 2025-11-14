93bd24a64ffbf23505165c8b1c183f5001533cf4
Jorge Matos <jisapjisap@gmail.com>
2025-11-14 13:39:18 +0100

    Update package dependencies for @radix-ui/react-label and react-hook-form; add Select and Textarea components for enhanced UI functionality; implement HttpRequestDialog for configuring HTTP requests in the workflow editor.

---
+++ src/features/executions/components/http-request/dialog.tsx
@@ -0,0 +1,73 @@
+"use client"
+
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import z from "zod"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { Textarea } from "@/components/ui/textarea"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { useEffect } from "react"
+
+
+
+const formSchema = z.object({
+  endpoint: z.url({ message: "Please enter a valid URL" }),
+  method: z.enum(["GET", "POST", "PUT", "DELETE", "PATCH"]),
+  body: z.string().optional(),
+})
+
+interface Props {
+  open: boolean
+  onOpenChange: (open: boolean) => void
+  onSubmit: (values: z.infer<typeof formSchema>) => void
+  defaultEndPoint?: string
+  defaultMethod?: "GET" | "POST" | "PUT" | "DELETE" | "PATCH"
+  defaultBody?: string
+}
+
+export const HttpRequestDialog = ({
+  open,
+  onOpenChange,
+}: Props) => {
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>HTTP Request</DialogTitle>
+          <DialogDescription>
+            Configure settings for the HTTP Request node.
+          </DialogDescription>
+        </DialogHeader>      
+
+      </DialogContent>
+    </Dialog>
+  )
+}
--- src/features/executions/components/http-request/node.tsx
+++ src/features/executions/components/http-request/node.tsx
@@ -4,6 +4,7 @@ import type { Node, NodeProps, useReactFlow } from "@xyflow/react";
 import { GlobeIcon } from "lucide-react";
 import { memo, useState } from "react";
 import { BaseExecutionNode } from "../base-execution-node";
+import { HttpRequestDialog } from "./dialog";
 
 type HttpRequestNodeData = {
   endpoint?: string;
@@ -15,15 +16,32 @@ type HttpRequestNodeData = {
 type HttpRequestNodeType = Node<HttpRequestNodeData>;
 
 export const HttpRequestNode = memo((props: NodeProps<HttpRequestNodeType>) => {
+
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  const nodeStatus = "initial";
+
+  const handleOpenSettings = () => {
+    setDialogOpen(true);
+  }
+
   const nodeData = props.data; 
   const description = nodeData?.endpoint
     ? `${nodeData.method || "GET"}: ${nodeData.endpoint}`
     : "Not configured"
 
-  const nodeStatus = "initial";
 
   return (
     <>
+      <HttpRequestDialog 
+        open={dialogOpen}
+        onOpenChange={setDialogOpen}
+        onSubmit={() => {}}
+        defaultEndPoint={nodeData.endpoint}
+        defaultMethod={nodeData.method}
+        defaultBody={nodeData.body}
+      />
+
       <BaseExecutionNode 
         {...props}
         id={props.id}
@@ -31,8 +49,8 @@ export const HttpRequestNode = memo((props: NodeProps<HttpRequestNodeType>) => {
         name="HTTP Request"
         status={nodeStatus}
         description={description}
-        onSettings={() => {}}
-        onDoubleClick={() => {}}
+        onSettings={handleOpenSettings}
+        onDoubleClick={handleOpenSettings}
       />
     </>
   )
