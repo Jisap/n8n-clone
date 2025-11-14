887444ab0104ae22615fb7093eb09dded59290ae
Jorge Matos <jisapjisap@gmail.com>
2025-11-14 15:06:59 +0100

    Implement HttpRequestDialog component with form handling for HTTP requests; add method, endpoint, and body fields with dynamic visibility based on selected method, enhancing user experience in the workflow editor.

--- src/features/executions/components/http-request/dialog.tsx
+++ src/features/executions/components/http-request/dialog.tsx
@@ -35,6 +35,8 @@ import { Textarea } from "@/components/ui/textarea"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { useEffect } from "react"
+import { Toaster as Sonner } from 'sonner';
+import { type NodeProps } from '@xyflow/react';
 
 
 
@@ -56,7 +58,31 @@ interface Props {
 export const HttpRequestDialog = ({
   open,
   onOpenChange,
+  onSubmit,
+  defaultEndPoint="",
+  defaultMethod="GET",
+  defaultBody="",
 }: Props) => {
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues:{
+      endpoint: defaultEndPoint,
+      method: defaultMethod,
+      body: defaultBody,
+    }
+  });
+
+  const watchMethod = form.watch("method");                             // Permite obtener el valor del campo "method" en el formulario
+  const showBodyField = ["POST", "PUT", "PATCH"].includes(watchMethod); // Muestra el campo "body" en función del valor del campo "method"
+
+  const handleSubmit = (values: z.infer<typeof formSchema>) => {
+    {
+      onSubmit(values);
+      onOpenChange(false);
+    }
+  }
+
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogContent>
@@ -66,6 +92,104 @@ export const HttpRequestDialog = ({
             Configure settings for the HTTP Request node.
           </DialogDescription>
         </DialogHeader>      
+
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(handleSubmit)}
+            className="space-y-8 mt-4"
+          >
+            <FormField 
+              control={form.control}
+              name="method"
+              render={({field}) => (
+                <FormItem>
+                  <FormLabel>Method</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger className="w-full">
+                        <SelectValue 
+                          placeholder="Select a method"
+                        />
+                      </SelectTrigger>
+                    </FormControl>
+
+                    <SelectContent>
+                      <SelectItem value="GET">GET</SelectItem>
+                      <SelectItem value="POST">POST</SelectItem>
+                      <SelectItem value="PUT">PUT</SelectItem>
+                      <SelectItem value="DELETE">DELETE</SelectItem>
+                      <SelectItem value="PATCH">PATCH</SelectItem>
+                    </SelectContent>
+                  </Select>
+
+                  <FormDescription>
+                    The HTTP method to use for this request
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="endpoint"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Endpoint URL</FormLabel>
+                  
+                  <FormControl>
+                    <Input 
+                      {...field}
+                      placeholder="https://api.example.com/users/{{httpResponse.data.id}}"
+                    />
+                  </FormControl>
+
+                  <FormDescription>
+                    Static URL or use {"{{variables}}"} for 
+                    simple values or {"{{json variable}}"} to 
+                    stringify objects
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {showBodyField && (
+              <FormField 
+                control={form.control}
+                name="body"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Request Body</FormLabel>
+
+                    <FormControl>
+                      <Textarea
+                        {...field}
+                        placeholder={
+                          '{\n "userId": "{{httpResponse.data.id}}",\n "name": "{{httpResponse.data.name}},"\n "items": "{{httpResponse.data.items}}"\n}'
+                        }
+                        className="min-h-[120px] font-mono text-sm"
+                      />
+                    </FormControl>
+
+                    <FormDescription>
+                      JSON with template variables. Use {"{{variables}}"} for
+                      simple values or {"{{json variable}}"} to
+                      stringify objects
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            )}
+            <DialogFooter className="mt-4">
+              <Button type="submit">Save</Button>
+            </DialogFooter>
+          </form>
+        </Form>
 
       </DialogContent>
     </Dialog>
