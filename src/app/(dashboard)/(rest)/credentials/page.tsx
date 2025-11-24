d940b59f390ad017e6340c63dceebf63746cddf0
Jorge Matos <jisapjisap@gmail.com>
2025-11-24 15:11:16 +0100

    Implement credentials page with parameter loading and prefetching; add server-side utilities for handling URL parameters and credential prefetching.

--- src/app/(dashboard)/(rest)/credentials/page.tsx
+++ src/app/(dashboard)/(rest)/credentials/page.tsx
@@ -1,12 +1,34 @@
+import { credentialsParamsLoader } from '@/features/credentials/server/params-loader';
+import { prefetchCredentials } from '@/features/credentials/server/prefetch';
 import { requireAuth } from '@/lib/auth-utils';
-import React from 'react'
+import { HydrateClient } from '@/trpc/server';
+import { SearchParams } from 'nuqs';
+import React, { Suspense } from 'react'
+import { ErrorBoundary } from 'react-error-boundary';
 
-const page = async() => {
+
+type Props = {
+  searchParams: Promise<SearchParams>
+}
+
+const page = async({ searchParams }: Props) => {
 
   await requireAuth();
 
+  const params = await credentialsParamsLoader(searchParams); // Carga los parámetros de la URL
+  prefetchCredentials(params); // Precarga las credenciales
+
+  
+
+
   return (
-    <div>Credentials page</div>
+    <HydrateClient>
+      <ErrorBoundary fallback={<div>Error</div>}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <p>TODO: Credentials page</p>
+        </Suspense>
+      </ErrorBoundary>
+    </HydrateClient>
   )
 }
 
