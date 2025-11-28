b788ed916ccb12cd9cae5d62c43ec2eff42dd744
Jorge Matos <jisapjisap@gmail.com>
2025-11-28 12:59:25 +0100

    Refactor executions page to include parameter loading and prefetching; integrate HydrateClient, ErrorBoundary, and Suspense for improved data handling

--- src/app/(dashboard)/(rest)/executions/page.tsx
+++ src/app/(dashboard)/(rest)/executions/page.tsx
@@ -1,12 +1,37 @@
+
+import { executionsParamsLoader } from '@/features/executions/server/params-loader';
+import { prefetchExecutions } from '@/features/executions/server/prefetch';
 import { requireAuth } from '@/lib/auth-utils';
+import { HydrateClient } from '@/trpc/server';
+import { SearchParams } from 'nuqs';
+import React, { Suspense } from 'react'
+import { ErrorBoundary } from 'react-error-boundary';
 
 
-const page = async() => {
+type Props = {
+  searchParams: Promise<SearchParams>
+}
+
+const page = async ({ searchParams }: Props) => {
 
   await requireAuth();
 
+  const params = await executionsParamsLoader(searchParams); // Carga los parámetros de la URL
+  prefetchExecutions(params); // Precarga las executions
+
+
+
+
   return (
-    <div>Executions page</div>
+    <>
+      <HydrateClient>
+        <ErrorBoundary fallback={<></>}>
+          <Suspense fallback={<></>}>
+            TODO: Listado de executions
+          </Suspense>
+        </ErrorBoundary>
+      </HydrateClient>
+    </>
   )
 }
 
